a14e9a4ed4074c6d324b4f071f6eb0c52cf7eece
anshul1301 <goyal.anshul@tftus.com>
2019-08-23 12:24:15 +0530

    first commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Main() {
+    return (
+        <p>This is main content block..</p>
+    )
+}
+
+export default Main
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function ToDo() {
+    return (
+        <div>
+            {/* style={{color:'red' ,backgroundColor:'yellow'}} */}
+            <input type="checkbox"/> 
+            <p  className="color">Placeholder text here</p>
+            <input type="checkbox"/> 
+            <p>Placeholder text here</p>
+            <input type="checkbox"/> 
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default ToDo
