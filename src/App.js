fff30adf4cdadea4423c17cd427a20853224db33
anshul1301 <goyal.anshul@tftus.com>
2019-08-27 11:41:19 +0530

    conditional styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Main from "./components/MainContent"
 import ToDo from "./components/ToDo"
 import Times from "./components/time"
 import ContactCard from "./components/ContactCard"
+import Joke from "./components/practise"
 
 function App() {
   return (
@@ -39,7 +40,7 @@ function App() {
     //   <ToDo/>
     // </div>
     <div>
-      <ContactCard
+      {/* <ContactCard
       name="Anshul"
       imgUrl="http://placekitten.com/300/200"
       phone="9874563210"
@@ -65,7 +66,21 @@ function App() {
       imgUrl="http://placekitten.com/200/100"
       phone="9874563210"
       email="abc@abc.com"
-      />
+      /> */}
+      <Joke
+        punchline="It's hard to explain xyz because they always take things literally."/>
+      <Joke 
+        question="what's the best thing about Switzerland" 
+        punchline="I don't know, but the flag is a big plus"/>
+      <Joke 
+        question="Did u hear about the Mathematician who's afraid of negative numbers?" 
+        punchline="He'll stop at nothing to avoid then!"/>
+      <Joke
+       question="Hear about the new resturant about karma?"
+       punchline="There's no menu: You get what you deserve."/>
+      <Joke
+        question="Did you hear about the actor who fell through the floorboards?"
+        punchline="he was just going through a stage."/>
     </div>
   );
 }
